07e5c2a0e5bd95fc837ff554ae90f0bc4b2d685a
John Paton <paton.john@kpmg.nl>
2018-05-29 15:48:18 +0200

    Tests for mongo auth credential pickup

--- subsidy_service/subsidy_service/config.py
+++ subsidy_service/subsidy_service/config.py
@@ -11,6 +11,66 @@ def set_to_default(cls):
     return cls
 
 
+def _get_mongo_uri(config: configparser.ConfigParser):
+    """Get the mongo host, port, and any credentials from the provided
+    config object or from the environment.
+
+    Returns a (uri, port) tuple, with the uri in the format
+    mongodb://[usr[:pwd]@]host
+
+    Preference is given to the config object. If no host is found,
+    returns (None, None).
+
+    """
+    try:
+        section = config['mongo']
+    except KeyError:
+        section = dict()
+
+    host = None
+    port = None
+
+    usr = None
+    pwd = None
+
+    if 'host' in section:
+        host = section['host']
+    elif 'MONGO_HOST' in os.environ:
+        host = os.environ['MONGO_HOST']
+    else:
+        return (None, None)
+
+    if 'port' in section:
+        port = section['port']
+    elif 'MONGO_PORT' in os.environ:
+        port = os.environ['MONGO_PORT']
+
+    if 'user' in section:
+        usr = section['user']
+    elif 'MONGO_USER' in os.environ:
+        usr = os.environ['MONGO_USER']
+
+    if 'password' in section:
+        pwd = section['password']
+    elif 'MONGO_PASSWORD' in os.environ:
+        pwd = os.environ['MONGO_PASSWORD']
+
+    uri = 'mongodb://'
+    if usr:
+        uri += usr  # -> mongodb://usr
+        if pwd:
+            # -> mongodb://usr:pwd
+            uri += ':' + pwd
+        uri += '@'  # -> mongodb://usr[:pwd]@
+
+    uri += host  # -> mongodb://[usr[:pwd]@]host
+
+    if port:
+        port = int(port)
+
+    return uri, port
+
+
 @set_to_default
 class Context():
     """
@@ -115,61 +175,4 @@ class Context():
 
         cls.read(config_path)
 
-
-
-def _get_mongo_uri(config: configparser.ConfigParser):
-    """Get the mongo host, port, and any credentials from the provided
-    config object or from the environment.
-
-    Returns a (uri, port) tuple, with the uri in the format
-    mongodb://[usr[:pwd]@]host
-
-    Preference is given to the config object. If no host is found,
-    returns (None, None).
-
-    """
-    try:
-        section = config['mongo']
-    except KeyError:
-        section = dict()
-
-    host = None
-    port = None
-
-    usr = None
-    pwd = None
-
-    if 'host' in section:
-        host = section['host']
-    elif 'MONGO_HOST' in os.environ:
-        host = os.environ['MONGO_HOST']
-    else:
-        return None
-
-    if 'port' in section:
-        port = section['port']
-    elif 'MONGO_PORT' in os.environ:
-        port = os.environ['MONGO_PORT']
-
-    if 'user' in section:
-        usr = section['user']
-    elif 'MONGO_USER' in os.environ:
-        usr = os.environ['MONGO_USER']
-
-    if 'password' in section:
-        pwd = section['password']
-    elif 'MONGO_PASSWORD' in os.environ:
-        pwd = os.environ['MONGO_PASSWORD']
-
-    uri = 'mongodb://'
-    if usr:
-        uri += usr  # -> mongodb://usr
-        if pwd:
-            # -> mongodb://usr:pwd
-            uri += ':' + pwd
-        uri += '@'  # -> mongodb://usr[:pwd]@
-
-    uri += host  # -> mongodb://[usr[:pwd]@]host
-
-    return uri, int(port)
 
--- subsidy_service/tests/test_config.py
+++ subsidy_service/tests/test_config.py
@@ -1,10 +1,23 @@
-from subsidy_service.config import Context
+from subsidy_service.config import Context, _get_mongo_uri
 import unittest
 from unittest import mock
 from . import common
 import collections
+import os
 
 DUMMY_CLIENT = collections.namedtuple('client', ['subsidy'])('test_db')
+DUMMY_CONFIG = {
+    'mongo': {
+        'host': 'hst1',
+        'port': '1231',
+        'user': 'usr1',
+        'password': 'pwd1'
+    }
+}
+
+DUMMY_ENVIRON = {'MONGO_HOST': 'hst2', 'MONGO_PORT': '1232',
+                 'MONGO_USER': 'usr2', 'MONGO_PASSWORD': 'pwd2'}
+
 
 @mock.patch('pymongo.MongoClient', new=common.dummy_func(DUMMY_CLIENT))
 @mock.patch('bunq.sdk.context.ApiContext.restore', new=common.dummy_func(None))
@@ -47,5 +60,64 @@ class TestContext(unittest.TestCase):
         self.assertIsNone(Context._last_read)
 
 
+class TestGetMongoURI(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.old_dummy_config = DUMMY_CONFIG.copy()
+        self.old_dummy_environ = DUMMY_ENVIRON.copy()
 
+        for k, v in DUMMY_ENVIRON.items():
+            os.environ[k] = v
+
+    def tearDown(self):
+        global DUMMY_CONFIG, DUMMY_ENVIRON
+        super().tearDown()
+        DUMMY_CONFIG = self.old_dummy_config
+        DUMMY_ENVIRON = self.old_dummy_environ
+
+        for k, v in DUMMY_ENVIRON.items():
+            try:
+                os.environ.pop(k)
+            except KeyError:
+                pass
+
+    def test_config_priority(self):
+        uri, port = _get_mongo_uri(DUMMY_CONFIG)
+        self.assertEqual('mongodb://usr1:pwd1@hst1', uri)
+        self.assertEqual(1231, port)
+
+    def test_environ(self):
+        uri, port = _get_mongo_uri({})
+        self.assertEqual('mongodb://usr2:pwd2@hst2', uri)
+        self.assertEqual(1232, port)
+
+    def test_no_user(self):
+        os.environ.pop('MONGO_USER')
+        uri, port = _get_mongo_uri({})
+        self.assertEqual('mongodb://hst2', uri)
+        self.assertEqual(1232, port)
+
+    def test_no_password(self):
+        os.environ.pop('MONGO_PASSWORD')
+        uri, port = _get_mongo_uri({})
+        self.assertEqual('mongodb://usr2@hst2', uri)
+        self.assertEqual(1232, port)
+
+    def test_no_host(self):
+        os.environ.pop('MONGO_HOST')
+        uri, port = _get_mongo_uri({})
+        self.assertIsNone(uri)
+        self.assertIsNone(port)
+
+    def test_no_port(self):
+        os.environ.pop('MONGO_PORT')
+        uri, port = _get_mongo_uri({})
+        self.assertEqual('mongodb://usr2:pwd2@hst2', uri)
+        self.assertIsNone(port)
+
+    def test_environ_fallback(self):
+        DUMMY_CONFIG['mongo'].pop('user')
+        uri, port = _get_mongo_uri(DUMMY_CONFIG)
+        self.assertEqual('mongodb://usr2:pwd1@hst1', uri)
+        self.assertEqual(1231, port)
 
