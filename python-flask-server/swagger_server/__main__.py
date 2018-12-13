37692402d9a64aa19b673d30a51fb3b131b0e36f
rvvincelli <Vincelli.Riccardo@KPMG.nl>
2018-12-13 15:55:08 +0100

    Enable CORS

--- python-flask-server/swagger_server/__main__.py
+++ python-flask-server/swagger_server/__main__.py
@@ -3,6 +3,7 @@ import warnings
 
 import connexion
 
+from flask_cors import CORS
 from swagger_server import encoder
 
 
@@ -13,6 +14,8 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'subsidy Allocation API'})
+
+    CORS(app.app) #https://github.com/zalando/connexion/issues/50
 
     # TODO: Improve SSL by generating and using non-adhoc certificates
     app.run(port=8080, debug=False)
